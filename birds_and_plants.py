278189f61757afba9f8a7898f7e42c7a3cf233f9
alercc <>
2024-12-08 17:15:12 -0300

    additional location; update of plants in former locations

--- birds_and_plants.py
+++ birds_and_plants.py
@@ -61,6 +61,27 @@ folium.Marker(
             <li>João-de-barro</li>
             <li>Tucano</li>
         </ul>
+        <p><b>Plantas:</b></p>
+        <ul>
+            <li>Cambará</li>
+            <li>Buriti</li>
+            <li>Flor de coral</li>
+            <li>Acalifa vermelha</li>
+            <li>Quaresmeira</li>
+            <li>Ipê amarelo</li>
+            <li>Ipê-amarelo-de-jardim</li>
+            <li>Ipê rosa</li>
+            <li>Mamão</li>
+            <li>Goiaba</li>
+            <li>Tábua-estreita</li>
+            <li>Taturana</li>
+            <li>Cana da Índia</li>
+            <li>Sibipiruna</li>
+            <li>Papiro</li>
+            <li>Escovinha-de-garrafa</li>
+            <li>Bela-emília</li>
+            <li>Jasmim-manga</li>
+        </ul>
     </div>
     """
     ).add_to(the_map)
@@ -99,9 +120,72 @@ folium.Marker(
             <li>Tico-tico</li>
             <li>Juriti-pupu</li>
         </ul>
+        <p><b>Plantas:</b></p>
+        <ul>
+            <li>Manduvi</li>
+            <li>Jaborandi-falso</li>
+            <li>Árvore guarda-chuva</li>
+            <li>Sisal</li>
+            <li>Aroeira-vermelha</li>
+            <li>Eucalipto</li>
+            <li>Chapéu-de-napoleão</li>
+            <li>Sansão do campo</li>
+            <li>Leucena</li>
+            <li>Mangueira</li>
+            <li>Paineira-do-cerrado</li>
+            <li>Margaridão</li>
+        </ul>
     </div>
     """
     ).add_to(the_map)
 
+# Parque da Cidade Sarah Kubitschek
+# _________________________________________________________________________
+# _________________________________________________________________________
+
+folium.Marker(
+    location=[-15.798027226292058, -47.89502115509866],
+    popup="""
+    <div style="
+        font-family: Arial, sans-serif;
+        color: black;
+        background-color: #f9f9f9;
+        padding: 10px;
+        border-radius: 8px;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+        width: 200px;
+    ">
+        <h3 style="color: #2c7efc;">Parque Vivencial do Lago Norte</h3>
+        <p><b>Pássaros:</b></p>
+        <ul>
+            <li>João-de-barro</li>
+            <li>Bem-te-vi</li>
+            <li>Sabiá-poca</li>
+            <li>Quero-quero</li>
+            <li>Pomba-asa-branca</li>
+            <li>Biguá</li>
+            <li>Socó-dorminhoco</li>
+            <li>Garça-vaqueira</li>
+            <li>Garça-branca-grande</li>
+            <li>Coruja-buraqueira</li>
+            <li>Periquito-de-encontro-amarelo</li>
+            <li>Canário-da-terra</li>
+            <li>Arara-canindé</li>
+            <li>Tesourinha</li>
+            <li>Cardeal-do-nordeste</li>
+        </ul>
+        <p><b>Plantas:</b></p>
+        <ul>
+            <li>Cedro-vermelho</li>
+            <li>Canafístula</li>
+            <li>Alamanda-amarela</li>
+            <li>Escovinha de garrafa</li>
+            <li>Ingá do mato</li>
+            <li>Manduvi</li>
+            <li>Mangueira</li>
+        </ul>
+    </div>
+    """
+    ).add_to(the_map)
 
 the_map.save("birds_and_plants.html")
